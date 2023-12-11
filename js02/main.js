758c5862803a5962b00ade092ce00eb52fffc25a
Sofia <petliovasofia@gmail.com>
2023-12-11 23:03:08 +0200

    Add part of h2

---
+++ js02/main.js
@@ -0,0 +1,16 @@
+var a = (5);  
+var b, c;
+
+b = ((a) * (5));  
+b = (c = (b/2));
+console.log(b);
+
+var a = 5;
+var b = a*5;
+var c = b/2;
+b = c;
+console.log(b);
+
+let Age = prompt ('Скільки вам років?');
++ Age;
+alert('Ваш рік народження: ' + (2023 - Age));
